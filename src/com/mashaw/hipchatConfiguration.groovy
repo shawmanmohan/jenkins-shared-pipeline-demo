f8192ab8cc941459d2dbef838013149205ba52cb
Manmohan Shaw <mashaw@hotwire.com>
2018-11-12 22:04:59 +0530

    Added more inputs

--- src/com/mashaw/hipchatConfiguration.groovy
+++ src/com/mashaw/hipchatConfiguration.groovy
@@ -1,1 +1,12 @@
+// This file define the Hipchat Server Configuration
+// We can setup up the configurations here directly or read from a text files in resources directory
+
 package com.mashaw
+
+class HipchatConfiguration{
+
+    static String HIPCHAT_SERVER_NAME="hipchat.mashaw.com"
+    static String HIPCHAT_SERVER_CREDENTIALS="975291db-b37c-42a9-a191-87f93502800a"
+    static String HIPCHAT_NOTIFICATION_USER="JENKINS_NOTIFIER"
+    
+}
